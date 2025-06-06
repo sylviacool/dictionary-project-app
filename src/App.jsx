de59a7b90647439dc4f54e69b8553250db98a92f
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2025-06-06 22:46:47 +0200

    Create the Search engine/Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Dictionary from "./components/Dictionary"
+import Dict from "./assets/Dict.jpg"
+import "./App.css"
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={Dict}
+            alt="Dictionary Image"
+            className="App-Img img-fluid"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>coded by Ogechi</small>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/Dictionary.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import './Dictionary.css';
+
+const Dictionary = () => {
+  const [keyword, setKeyword] = useState("");
+
+  const search = (event) => {
+    event.preventDefault();
+    alert(`Searching for: ${keyword}`);
+  }
+
+  const handleKeywordChange = (event) => {
+    setKeyword(event.target.value)
+  }
+  return <div className="Dictionary">
+    <form onSubmit={search}>
+      <input type="search" autoFocus={true} onChange={handleKeywordChange}/>
+    </form>
+  </div>;
+}
+
+export default Dictionary
